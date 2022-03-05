218fd5cc27b20d39b73ce37e219f8d4c5c150c03
Juan Muniain Otero <muniainjuan@icloud.com>
2022-03-05 13:15:01 -0600

    Changed movement to depend on the player’s velocity instead of force

--- Assets/Scripts/ShipMotion.cs
+++ Assets/Scripts/ShipMotion.cs
@@ -5,17 +5,15 @@ using UnityEngine;
 public class ShipMotion : MonoBehaviour
 {
     [SerializeField] float force; //crea un field (valor)
-    Rigidbody2D rb2D; //creas un componente de rigidbody para modificar
-    Vector3 move; //creas otro componente para que se mueva
+    Rigidbody2D rb2D; //crea un componente de rigidbody para modificar
+    Vector3 move; //crea otro componente para que se mueva
 
-    // Start is called before the first frame update
-    void Start() //función que se llama al iniciar el juego
+    void Awake()
     {
         rb2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update() //cada frame que pasa se actualiza y se llama a esta función 
+    void FixedUpdate()
     {
         //read user input from keyboard or gamepad
         move.x = Input.GetAxis("Horizontal") * force;
